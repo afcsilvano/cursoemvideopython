6f721f9ebc7b188f3d1b7618728ce01884ebec30
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2023-10-01 12:59:45 -0300

    Create ex072.py

---
+++ ex072.py
@@ -0,0 +1,4 @@
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+           'treze', 'catorze', 'quize', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+num = int(input('Digite um número entre 0 e 20: '))
+print('{} por extenso é {}'.format(num, (numeros[num])))

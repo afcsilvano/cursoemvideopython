0cb07094f53bfa902d6836a3826e09376400b4d2
Matheus Silvano Pereira <115375444+matheussilvano@users.noreply.github.com>
2023-10-01 17:27:24 -0300

    Exercício 072

--- ex072.py
+++ ex072.py
@@ -1,3 +1,4 @@
+#numeros por escrito
 numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
            'treze', 'catorze', 'quize', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 num = int(input('Digite um número entre 0 e 20: '))
